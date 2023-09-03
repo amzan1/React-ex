67e54ed6da5ae04e55905538f3615fb044f06ec4
amzan1 <amzanabdullah97@gmail.com>
2023-09-03 15:00:00 +0300

    z

--- my-react-ex/src/pages/ViewProd.tsx
+++ my-react-ex/src/pages/ViewProd.tsx
@@ -38,23 +38,9 @@ function ViewProd() {
         <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
           <div className="flex">
             
-            <div className="relative">
-              <select className="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
-                <option>SM</option>
-                <option>M</option>
-                <option>L</option>
-                <option>XL</option>
-              </select>
-              <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4" viewBox="0 0 24 24">
-                  <path d="M6 9l6 6 6-6"></path>
-                </svg>
-              </span>
-            </div>
-          </div>
-        </div>
+            
         <div className="flex">
-          <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
+          <span className="title-font font-medium text-2xl text-gray-900">${id.price}</span>
           <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Button</button>
           <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
